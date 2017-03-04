0ca73178eb138bce64ec50b48555328dae820a18
Romel Pérez <ronelprhone@gmail.com>
2017-03-04 11:40:34 -0500

    [update] play

--- play/server/index.js
+++ play/server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(`${process.cwd()}/public`));
 
 app.listen(port, function (err) {
